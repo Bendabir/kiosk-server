3b72e2e2c7cbb9b07c7497251157484581cc5f6a
Benjamin Rivière <briviere01@gmail.com>
2019-12-10 20:41:40 +0100

    Change log level for database.

--- src/connections/database.ts
+++ src/connections/database.ts
@@ -1,11 +1,10 @@
 import { Sequelize } from "sequelize";
-
 import * as config from "../config";
 import { logger } from "../logging";
 
 const uri = `postgres://${config.PG_USER}:${config.PG_PASS}@${config.PG_HOST}:${config.PG_PORT}/${config.PG_DB}`;
 const sequelize = new Sequelize(uri, {
-    logging: (msg) => logger.debug(msg),
+    logging: (msg) => logger.silly(msg),
     pool: {
         acquire: 30000,
         idle: 10000,
