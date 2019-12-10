57645bc1878b6c803871ac533ad9884bdf960419
Benjamin Rivière <briviere01@gmail.com>
2019-12-10 19:03:16 +0100

    Add nice header to app.

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // Extracting the env variables and storing the config
+export const VERSION = "3.0.0-alpha";
 export const LOG_LEVEL = process.env.LOG_LEVEL || "info";
 export const PORT = parseInt(process.env.PORT, 10) || 5000;
