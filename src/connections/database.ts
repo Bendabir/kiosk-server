1abf353e264287f8bf0f1fa2014f8d73c431ed3d
Benjamin Rivière <briviere01@gmail.com>
2019-12-10 19:33:21 +0100

    Add database support.

---
+++ src/connections/database.ts
@@ -0,0 +1,15 @@
+import { Sequelize } from "sequelize";
+
+import * as config from "../config";
+import { logger } from "../logging";
+
+const uri = `postgres://${config.PG_USER}:${config.PG_PASS}@${config.PG_HOST}:${config.PG_PORT}/${config.PG_DB}`;
+export const sequelize = new Sequelize(uri, {
+    logging: (msg) => logger.debug(msg),
+    pool: {
+        acquire: 30000,
+        idle: 10000,
+        max: 5,
+        min: 0
+    }
+});
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import * as config from "./config";
+import { sequelize } from "./connections/database";
 import { logger } from "./logging";
 
 logger.info("_   ___           _     ");
@@ -11,5 +12,17 @@ logger.info("");
 logger.info("Contribute : https://github.com/bendabir/kiosk-server");
 logger.info("");
 logger.info("Using the following configuration :");
-logger.info("    LOG_LEVEL = " + config.LOG_LEVEL);
-logger.info("    PORT      = " + config.PORT);
+logger.info("    LOG_LEVEL     = " + config.LOG_LEVEL);
+logger.info("    SERVER_HOST   = " + config.SERVER_HOST);
+logger.info("    SERVER_PORT   = " + config.SERVER_PORT);
+logger.info("    POSTGRES_HOST = " + config.PG_HOST);
+logger.info("    POSTGRES_PORT = " + config.PG_PORT);
+logger.info("    POSTGRES_USER = " + config.PG_USER);
+logger.info("    POSTGRES_DB   = " + config.PG_DB);
+
+sequelize.authenticate().then(() => {
+    logger.info("Connection to database successful !");
+}).catch((error) => {
+    logger.error("Error connecting to database : " + error.message);
+    process.exit(1);
+});
