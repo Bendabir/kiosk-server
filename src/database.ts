c3e4c053a1b9bb30ac7313f8a24b1cfcd8d53f03
Benjamin Rivière <briviere01@gmail.com>
2019-12-12 21:03:57 +0100

    Simplify code a bit.

--- src/database.ts
+++ src/database.ts
@@ -3,7 +3,7 @@ import * as config from "./config";
 import { logger } from "./logging";
 
 const uri = `postgres://${config.PG_USER}:${config.PG_PASS}@${config.PG_HOST}:${config.PG_PORT}/${config.PG_DB}`;
-const sequelize = new Sequelize(uri, {
+export const sequelize = new Sequelize(uri, {
     logging: (msg) => logger.silly(msg),
     pool: {
         acquire: 30000,
@@ -19,5 +19,3 @@ sequelize.authenticate().then(() => {
     logger.error("Error connecting to database : " + error.message);
     process.exit(1);
 });
-
-export { sequelize };
