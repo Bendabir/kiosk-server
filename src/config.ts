0e487d7c421adb6f76e6036b6f27563a4e445b62
Benjamin Rivière <briviere01@gmail.com>
2019-12-17 08:28:06 +0100

    Change default host.

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ dotenv.config();
 // Extracting the env variables and storing the config
 export const VERSION = "3.0.0-alpha";
 export const LOG_LEVEL = process.env.LOG_LEVEL || "info";
-export const SERVER_HOST = process.env.SERVER_HOST || "127.0.0.1";
+export const SERVER_HOST = process.env.SERVER_HOST || "0.0.0.0";
 export const SERVER_PORT = parseInt(process.env.SERVER_PORT, 10) || 5000;
 
 // For the database
