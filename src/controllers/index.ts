4b3ca08344c4bfa6c954b88f5a20c6aa06b29ff8
Benjamin Riviere <benjamin.riviere@linkfluence.com>
2019-12-23 12:07:00 +0100

    Add controller for groups.

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,7 +1,9 @@
 import { ContentsController } from "./contents.controller";
+import { GroupsController } from "./groups.controller";
 import { TVsController } from "./tvs.controller";
 
 export {
     ContentsController,
+    GroupsController,
     TVsController
 };
